427b8c08ebbfc5e4d15fd3662e3e2efc3af4758c
bhuwan <ricky.pantha@gmail.com>
2024-11-16 11:11:36 +0545

    initialized

---
+++ auth/app.py
@@ -0,0 +1,59 @@
+"""Server app config."""
+
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+from beanie import init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient
+from starlette.middleware.cors import CORSMiddleware
+
+from auth.config import CONFIG
+from auth.models.user import User
+
+from auth.routes.auth import router as AuthRouter
+from auth.routes.mail import router as MailRouter
+from auth.routes.register import router as RegisterRouter
+from auth.routes.user import router as UserRouter
+
+
+DESCRIPTION = """
+This API powers whatever I want to make
+
+It supports:
+
+- Account sign-up and management
+- Something really cool that will blow your socks off
+"""
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):  # type: ignore
+    """Initialize application services."""
+    app.db = AsyncIOMotorClient(CONFIG.mongo_uri).account  # type: ignore[attr-defined]
+    await init_beanie(app.db, document_models=[User])  # type: ignore[arg-type,attr-defined]
+    print("Startup complete")
+    yield
+    print("Shutdown complete")
+
+
+app = FastAPI(
+    title="My Server",
+    description=DESCRIPTION,
+    version="0.1.0",
+    lifespan=lifespan,
+)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+app.include_router(AuthRouter)
+app.include_router(MailRouter)
+app.include_router(RegisterRouter)
+app.include_router(UserRouter)
---
+++ auth/jwt.py
@@ -0,0 +1,39 @@
+"""FastAPI JWT configuration."""
+
+from datetime import timedelta
+import jwt
+from fastapi_jwt import JwtAuthorizationCredentials, JwtAccessBearer, JwtRefreshBearer
+
+from auth.config import CONFIG
+from auth.models.user import User
+
+ACCESS_EXPIRES = timedelta(minutes=15)
+REFRESH_EXPIRES = timedelta(days=30)
+
+access_security = JwtAccessBearer(
+    CONFIG.authjwt_secret_key,
+    access_expires_delta=ACCESS_EXPIRES,
+    refresh_expires_delta=REFRESH_EXPIRES,
+)
+
+refresh_security = JwtRefreshBearer(
+    CONFIG.authjwt_secret_key,
+    access_expires_delta=ACCESS_EXPIRES,
+    refresh_expires_delta=REFRESH_EXPIRES,
+)
+
+
+async def user_from_credentials(auth: JwtAuthorizationCredentials) -> User | None:
+    """Return the user associated with auth credentials."""
+    return await User.by_email(auth.subject["username"])
+
+
+async def user_from_token(token: str) -> User | None:
+    """Return the user associated with a token value."""
+    payload = await decode_token(token)
+    if payload:
+        return await User.by_email(payload["subject"]["username"])
+
+
+async def decode_token(token: str) -> dict:
+    return jwt.decode(token, CONFIG.authjwt_secret_key, algorithms=["HS256"])
