1ba8e44c272147a10079dd083dfe219d061a3c8e
bhuwan <ricky.pantha@gmail.com>
2025-02-16 23:21:28 +0545

    refactor

--- auth/models/flight_record.py
+++ auth/models/flight_record.py
@@ -1,31 +1,10 @@
 from beanie import Document, PydanticObjectId
 from datetime import datetime
 
-from pydantic import BaseModel, Field, field_validator, validator
+from pydantic import BaseModel, Field, field_validator
 
 from bson import ObjectId
-from bson import ObjectId
 
-class PydanticObjectId(ObjectId):
-    @classmethod
-    def __get_validators__(cls):
-        yield cls.validate
-
-    @classmethod
-    def validate(cls, value, validation_info=None):
-        if not (
-            value
-            and (isinstance(value, ObjectId) or isinstance(value, str))
-            and ObjectId().is_valid(value)
-        ):
-            raise ValueError("Not a valid ObjectId")
-
-        return value
-
-    @classmethod
-    def __get_pydantic_json_schema__(cls, field_schema, handler):
-        new_field_schema = {"type": "string", "examples": ["66488b368a6801e71d70dfe9"]}
-        return new_field_schema
 
 
 class FlightRecordIn(BaseModel):
